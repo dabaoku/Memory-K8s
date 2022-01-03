294a16f126fad8caa8002bbee94accfbea96aa27
yucheng880730 <yucheng2k13@gmail.com>
2022-01-03 13:05:37 +0800

    fix warning logs

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,6 +4,9 @@ export default (posts = [], action) => {
     case "DELETE":
       return posts.filter((post) => post._id !== action.payload);
     case "UPDATE":
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
     case "LIKE":
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
